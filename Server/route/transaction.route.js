d518c63603813dec82de2af8613928dc6c131b57
fahama-globiance <hkh@globiancegroup.com>
2024-05-14 14:39:56 +0700

    webhook configured

--- Server/route/transaction.route.js
+++ Server/route/transaction.route.js
@@ -5,5 +5,6 @@ const transactionController = require('../controller/transaction.controller');
 const authenticateToken = require('../middleware/authentication'); 
 
 router.get('/', authenticateToken, transactionController.getTransactions);
+router.post('/payment', transactionController.paymentWebhook);
 
 module.exports = router;
