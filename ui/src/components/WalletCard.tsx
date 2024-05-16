7fcffb359e82c30630b79db3fc80447ea5759393
Itthipol Lateh <nil@globiancegroup.com>
2024-05-16 12:39:40 +0700

    add a refresh button

--- ui/src/components/WalletCard.tsx
+++ ui/src/components/WalletCard.tsx
@@ -1,13 +1,22 @@
+import { useWallet } from "@/hooks/useWallet";
+import { faRefresh } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 interface IWalletCard {
   balance: number;
 }
 
 export default function WalletCard({ balance }: IWalletCard) {
+  const { refetch } = useWallet()
+
   return (
     <div className="wallet-card">
       <div className="wallet-content">
         <h3 className="xl:h3 lg:h4 sm:h5 xs:h6">My wallet balance</h3>
-        <h3 className="xl:h3 lg:h4 sm:h5 xs:h6">$ {balance ? Number(balance).toFixed(2) : ""}</h3>
+        <span className="xl:h3 lg:h4 sm:h5 xs:h6">$ {balance ? Number(balance).toFixed(2) : ""}
+          <span onClick={() => {refetch()}} className="ml-5 cursor-pointer"><FontAwesomeIcon className="hover:shadow-xl" icon={faRefresh} />
+          </span>
+        </span>
       </div>
     </div>
   );
